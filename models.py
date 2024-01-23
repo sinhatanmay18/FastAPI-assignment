f45a90e62bd1a2e9a14d742575a331f1fdfc123e
Tanmay Sinha <sinha.t18@outlook.com>
2024-01-23 18:55:52 +0530

    Update models.py
    adjusted request body for 100 x 100 size matrix

--- models.py
+++ models.py
@@ -6,6 +6,6 @@ class Log(Base):
     __tablename__ = "logs"
 
     id = Column(Integer, primary_key=True, index=True)
-    request_body = Column(String(250))
+    request_body = Column(String(20000))
     response_body = Column(String(250))
     processing_time = Column(Float)
